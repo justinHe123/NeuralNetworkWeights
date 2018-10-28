5c747977d03220eee74ea9fae9a642a618650a71
young <youngyu2002@gmail.com>
2018-10-27 19:57:58 -0700

    Initialized variables and data.

--- src/neuralNetwork/Driver.java
+++ src/neuralNetwork/Driver.java
@@ -1,4 +1,11 @@
 package neuralNetwork;
 
 public class Driver {
+
+    public static void main(String[] args) {
+        int[][][] data = Perceptron.andData;
+        double[] weights = Perceptron.INITIAL_WEIGHTS;
+        Driver driver = new Driver();
+        Perceptron perceptron = new Perceptron();
+    }
 }
