df7b765e12fc5684533f124fdaad09cf26101e11
young <youngyu2002@gmail.com>
2018-10-27 19:53:02 -0700

    An artificial neuron modeling a biological neuron.

---
+++ src/neuralNetwork/Perceptron.java
@@ -0,0 +1,9 @@
+package neuralNetwork;
+
+public class Perceptron {
+    // training data
+    public static final int[][][] andData = {{{0, 0}, {0}},
+            {{0, 1}, {0}},
+            {{1, 0}, {0}},
+            {{1, 1}, {1}}};
+}
